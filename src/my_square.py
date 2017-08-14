212243137e6589e6b1e1126deff7c02fb568ea9e
madhukavali123 <madhu.kavali@gmail.com>
2017-08-13 19:47:25 -0700

    Changed the input parameter and the variable to square

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,8 @@
-def my_square(x):
+def my_square(z):
 	""" takes a number and returns the square of the number
 	uses "" operator
 	"""
-	return (x ** 2)
+	return (z * z)
 
-print(my_square(4))
+print(my_square(24))
 
